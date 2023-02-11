a8becc4a13703fbcf76fbe5f87e9334449a5d42c
roberte777 <robewilkes@yahoo.com>
2023-02-11 17:32:16 -0600

    spawnig multiple boids

--- src/components/Boids/School.tsx
+++ src/components/Boids/School.tsx
@@ -1,28 +1,27 @@
+import type { ThreeElements } from "@react-three/fiber";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
 export const School = () => {
-  const ref = useRef<THREE.Mesh>(null);
-  useFrame((state, delta) => {
-    ref.current?.translateX(0.01);
-  });
-  return (
-    <mesh ref={ref}>
-      <coneGeometry args={[0.5, 1]} />
-      <meshBasicMaterial color="royalblue" />
-    </mesh>
-  );
+    const ref = useRef<THREE.Mesh>(null);
+    useFrame((state, delta) => {
+        ref.current?.translateX(0.01);
+    });
+    return (
+        <group ref={ref}>
+            {Array.from({ length: 100 }).map((_, i) => (
+                <Boid key={i} position={[i, 0, 0]} />
+            ))}
+        </group>
+    );
 };
 
-const Boid = () => {
-  const ref = useRef<THREE.Mesh>(null);
-  useFrame((state, delta) => {
-    ref.current?.translateX(0.01);
-  });
-  return (
-    <mesh ref={ref}>
-      <coneGeometry args={[0.5, 1]} />
-      <meshBasicMaterial color="royalblue" />
-    </mesh>
-  );
+const Boid = (props: ThreeElements["mesh"]) => {
+    const ref = useRef<THREE.Mesh>(null);
+    return (
+        <mesh ref={ref} {...props}>
+            <coneGeometry args={[0.5, 1]} />
+            <meshBasicMaterial color="royalblue" />
+        </mesh>
+    );
 };
