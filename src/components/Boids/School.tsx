502ef164c000f2c047bab76824acbe0ff83004ea
roberte777 <robewilkes@yahoo.com>
2023-02-11 18:41:33 -0600

    added rigid body

--- src/components/Boids/School.tsx
+++ src/components/Boids/School.tsx
@@ -1,27 +1,35 @@
 import type { ThreeElements } from "@react-three/fiber";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
-import { RigidBody } from "@react-three/rapier";
+import { RapierRigidBody, RigidBody } from "@react-three/rapier";
 
 export const School = () => {
   const ref = useRef<THREE.Group>(null);
   return (
     <group ref={ref}>
-      {Array.from({ length: 10 }).map((_, i) => (
-        <Boid key={i} position={[i, 0, 0]} />
-      ))}
+      {Array.from({ length: 10 }).map((_, i) => {
+        let randomX = Math.random() * 10;
+        let randomY = Math.random() * 10;
+        let randomZ = Math.random() * 10;
+        return <Boid key={i} position={[randomX, randomY, randomZ]} />;
+      })}
     </group>
   );
 };
 
 const Boid = (props: ThreeElements["mesh"]) => {
-  const ref = useRef<THREE.Mesh>(null);
+  const ref = useRef<RapierRigidBody>(null);
   useFrame((state, delta) => {
-    ref.current?.translateX(0.01);
+    if (ref.current) {
+      let vel = ref.current.linvel();
+      ref.current.setRotation([0, 0.5, 5]);
+      //get the current velocity and rotate the boid towards it
+      ref.current?.setLinvel({ x: 1, y: 0, z: 0 });
+    }
   });
   return (
-    <RigidBody>
-      <mesh ref={ref} {...props}>
+    <RigidBody ref={ref} {...props}>
+      <mesh>
         <coneGeometry args={[0.5, 1]} />
         <meshBasicMaterial color="royalblue" />
       </mesh>{" "}
