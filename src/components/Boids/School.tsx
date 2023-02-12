bc9f1b3c5c549ec8e475b9d7a233867f9fd35f83
roberte777 <robewilkes@yahoo.com>
2023-02-11 23:49:37 -0600

    fixed Carlos's shit

--- src/components/Boids/School.tsx
+++ src/components/Boids/School.tsx
@@ -1,31 +1,37 @@
-import * as THREE from "three";
-import type { ConeGeometryProps, ThreeElements } from "@react-three/fiber";
+import type * as THREE from "three";
 import { useFrame } from "@react-three/fiber";
-import { Cone, Cylinder } from "@react-three/drei";
+import { Cone } from "@react-three/drei";
 import { useEffect, useRef } from "react";
 import {
-  RapierRigidBody,
-  RigidBodyProps,
-  euler,
+  type RapierRigidBody,
+  type RigidBodyProps,
   vec3,
 } from "@react-three/rapier";
-import { Debug, quat } from "@react-three/rapier";
-import { CylinderCollider } from "@react-three/rapier";
+import { Debug } from "@react-three/rapier";
 import { RigidBody } from "@react-three/rapier";
-import { Quaternion, Vector3 } from "three";
-import { useControls } from "leva";
+import { Vector3 } from "three";
+// import { useControls } from "leva";
 
 export const School = () => {
   const ref = useRef<THREE.Group>(null);
 
-  return <Boid />;
+  return (
+    <group ref={ref}>
+      {[...(Array(10) as number[])].map((_, i) => {
+        const randomX = Math.random() * 10;
+        const randomY = Math.random() * 10;
+        const randomZ = Math.random() * 10;
+        return <Boid key={i} position={[randomX, randomY, randomZ]} />;
+      })}
+    </group>
+  );
 };
 
 const Boid = (props: RigidBodyProps) => {
   const ref = useRef<RapierRigidBody>(null);
   const coneRef = useRef<THREE.Mesh>(null);
 
-  // const { x, y, z, lookX, lookY, lookZ, velocity } = useControls({
+  // const {x, y, z, lookX, lookY, lookZ, velocity} = useControls({
   //   x: { value: 0, min: 0, max: 50, step: Math.PI / 6 },
   //   y: { value: 0, min: 0, max: 50, step: Math.PI / 6 },
   //   z: { value: 0, min: 0, max: 50, step: Math.PI / 6 },
@@ -39,8 +45,8 @@ const Boid = (props: RigidBodyProps) => {
     coneRef.current?.geometry.rotateX(Math.PI / 2);
   }, []);
 
-  useFrame((state, delta) => {
-    if (ref.current) {
+  useFrame(() => {
+    if (ref.current && coneRef.current) {
       let target = new Vector3();
       target = coneRef.current?.getWorldPosition(target);
 
@@ -53,10 +59,9 @@ const Boid = (props: RigidBodyProps) => {
   return (
     <>
       <RigidBody ref={ref} {...props} linearVelocity={[0.1, 0.1, 0.1]}>
-        <Cone ref={coneRef} args={[1, 2, 20]}>
+        <Cone ref={coneRef} args={[0.5, 1, 20]}>
           <meshPhysicalMaterial color="orange" />
         </Cone>
-        {/* <CylinderCollider position={[0, -2, 0]} args={[20, 0.5, 20]} /> */}
       </RigidBody>
       <Debug />
     </>
